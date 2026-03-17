f22f6a5c58a31aacdedb375cea893483ff3b7f2b
youpzdev <danilgvizdon@mail.ru>
2026-03-17 12:06:32 +0300

    Custom Pooling

--- Assets/Import/youpzdev/Goofy-Pooling/Scripts/Pooling.cs
+++ Assets/Import/youpzdev/Goofy-Pooling/Scripts/Pooling.cs
@@ -7,7 +7,7 @@ public static class Pooling
     private static readonly Dictionary<GameObject, GameObject> prefabMap = new();
 
     // ====== Methods ====================================================================================
-    public static GameObject Instantiate(GameObject prefab, Vector3 position, Quaternion rotation)
+    public static GameObject Instantiate(GameObject prefab, Vector3 position = default, Quaternion rotation = default, Transform parent = null)
     {
         if (!pools.ContainsKey(prefab)) pools[prefab] = new Queue<GameObject>();
 
@@ -25,8 +25,16 @@ public static class Pooling
             prefabMap[obj] = prefab;
         }
 
+        if (parent != null) obj.transform.SetParent(parent);
+
         return obj;
     }
+
+    public static GameObject Instantiate(GameObject prefab, Transform parent)
+    {
+        return Instantiate(prefab, Vector3.zero, Quaternion.identity, parent);
+    }
+
     public static void Destroy(GameObject obj)
     {
         if (!prefabMap.TryGetValue(obj, out GameObject prefab)) return;
--- Assets/Scripts/Core Things/User Interface/Buildings Panels/BuildingInfoPanel.cs
+++ Assets/Scripts/Core Things/User Interface/Buildings Panels/BuildingInfoPanel.cs
@@ -6,15 +6,17 @@ public class BuildingInfoPanel : MonoBehaviour
 {
     [SerializeField] private TMP_Text titleText;
     [SerializeField] private TMP_Text upgradeTitleText;
-    [Space(5)] [Header("Stats Grid")]
+    [Space(5)]
+    [Header("Stats Grid")]
     [SerializeField] private Transform statsGrid;
     [SerializeField] private Transform upgradeGrid;
     [SerializeField] private GameObject statsPrefab;
-    
-    [Space(10)] [Header("References")]
+
+    [Space(10)]
+    [Header("References")]
     [SerializeField] private UIPanel uiPanel;
-    [SerializeField] private Button upgradeButton; // прикрутить потом обновление кликабельности на кол-во текущей валюты
-    [SerializeField] private TMP_Text upgradeButtonText; 
+    [SerializeField] private Button upgradeButton;
+    [SerializeField] private TMP_Text upgradeButtonText;
 
     private BuildingData currentData;
     private ResourceGainer currentGainer;
@@ -22,7 +24,7 @@ public class BuildingInfoPanel : MonoBehaviour
 
     void Start()
     {
-        if (!uiPanel) uiPanel = GetComponent<UIPanel>(); // подстраховка 
+        if (!uiPanel) uiPanel = GetComponent<UIPanel>();
         upgradeButton.onClick.AddListener(UpgradeBuilding);
     }
 
@@ -34,8 +36,8 @@ public class BuildingInfoPanel : MonoBehaviour
         currentGainer = gainer;
         level = gainer.Level;
 
-        foreach (Transform obj in statsGrid) Destroy(obj.gameObject);
-        foreach (Transform obj in upgradeGrid) Destroy(obj.gameObject);
+        foreach (Transform obj in statsGrid) Pooling.Destroy(obj.gameObject);
+        foreach (Transform obj in upgradeGrid) Pooling.Destroy(obj.gameObject);
 
         titleText.text = currentData.buildingName + $" | Level {level}";
         upgradeTitleText.text = "";
@@ -43,7 +45,7 @@ public class BuildingInfoPanel : MonoBehaviour
 
         GainResources targetSlot = currentData.gainResources[level - 1];
 
-        StatsHolder statsHolder = Instantiate(statsPrefab, statsGrid).GetComponent<StatsHolder>();
+        StatsHolder statsHolder = Pooling.Instantiate(statsPrefab, statsGrid).GetComponent<StatsHolder>();
         float producePerSec = (float)targetSlot.gainLimit / targetSlot.gainTime;
         statsHolder.Init(UIManager.Instance.GetIcon(targetSlot.resourceType), producePerSec, targetSlot.gainLimit);
 
@@ -55,12 +57,12 @@ public class BuildingInfoPanel : MonoBehaviour
             upgradeButton.interactable = ResourceManager.Instance.CanSpendResource(targetSlot.upgradePrice.ResourceType, targetSlot.upgradePrice.Amount);
             upgradeButtonText.text = $"Upgrade\n {targetSlot.upgradePrice.Amount} {targetSlot.upgradePrice.ResourceType}";
             upgradeTitleText.text = $"Level {level + 1}";
-            
-            StatsHolder updateHolder = Instantiate(statsPrefab, upgradeGrid).GetComponent<StatsHolder>();
+
+            StatsHolder updateHolder = Pooling.Instantiate(statsPrefab, upgradeGrid).GetComponent<StatsHolder>();
             producePerSec = (float)targetSlot.gainLimit / targetSlot.gainTime;
             updateHolder.Init(UIManager.Instance.GetIcon(targetSlot.resourceType), producePerSec, targetSlot.gainLimit);
         }
-        
+
         uiPanel.Show();
     }
 
@@ -80,5 +82,4 @@ public class BuildingInfoPanel : MonoBehaviour
         currentGainer.AddLevel();
         Show(currentGainer);
     }
-
 }
